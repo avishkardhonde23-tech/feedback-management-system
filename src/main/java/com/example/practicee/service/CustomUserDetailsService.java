8e4ce0a2144755dd6f4121a46f3bf54d51228a8a
avishkar <avishkardhonde23@gmail.com>
2026-06-20 18:16:54 +0530

    Configure Render environment variables

---
+++ src/main/java/com/example/practicee/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.practicee.service;
+
+import com.example.practicee.entity.UserEntity;
+import com.example.practicee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(
+                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
+                )
+        );
+    }
+}
--- src/main/java/com/example/practicee/service/UserService.java
+++ src/main/java/com/example/practicee/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.practicee.service;
 import com.example.practicee.entity.UserEntity;
 import com.example.practicee.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,15 @@ public class UserService {
 
     @Autowired
     private UserRepository uRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserEntity registerUser(UserEntity user){
         if(uRepo.findByEmail(user.getEmail()).isPresent()){
             throw new RuntimeException("Email already registered");
         }
         user.setRole("USER");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return uRepo.save(user);
     }
 
